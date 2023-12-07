c37bd36a7744792cfa6bb5f11cf8a5b0c3a8c228
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-07 10:32:43 -0800

    Update Array-deep-count.js

--- Array-deep-count.js
+++ Array-deep-count.js
@@ -14,3 +14,13 @@ Examples:
 
 
 // Solution
+
+function deepCount(a) {
+  let count = a.length;
+  for (let i = 0; i < a.length; i++) {
+    if (Array.isArray(a[i])) {
+      count += deepCount(a[i]);
+    }
+  }
+  return count;
+}
